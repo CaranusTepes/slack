2edd9a409e9afdc34a45ef59c33fb0cdf9033f8e
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-04-09 16:38:28 +0800

    added routes and login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Data from "./Data";
+import Login from "./Pages/Login/Login"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/home" element={<Data />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
